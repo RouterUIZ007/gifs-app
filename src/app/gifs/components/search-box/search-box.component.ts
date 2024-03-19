b752602b0a8fe959bc53d10928cadd6af18b68a6
RouterUIZ007 <router.uiz.360@gmail.com>
2024-03-19 00:19:37 -0600

    feat(page): Feat add shared and gifs

---
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'gifs-search-box',
+    templateUrl: 'search-box.component.html'
+})
+
+export class SearchBoxComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
